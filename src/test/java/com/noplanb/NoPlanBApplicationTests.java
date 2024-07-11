e65b2080ca32493e1419176af08782ecb38b0023
EunbeenDev <ros4312@naver.com>
2024-07-11 14:39:55 +0900

    initial commit

---
+++ src/test/java/com/noplanb/NoPlanBApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noplanb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoPlanBApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
