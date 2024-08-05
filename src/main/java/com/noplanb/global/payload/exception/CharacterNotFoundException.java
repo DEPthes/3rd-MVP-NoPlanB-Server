8d8e14bc38ff562ae1d2baca4c1058bcf030d23a
lsn5963 <lsn5963@naver.com>
2024-08-05 18:55:33 +0900

    [FEAT] 퀘스트 조회,추가, 에러코드 핸들링

---
+++ src/main/java/com/noplanb/global/payload/exception/CharacterNotFoundException.java
@@ -0,0 +1,11 @@
+package com.noplanb.global.payload.exception;
+
+import com.noplanb.global.payload.ErrorCode;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class CharacterNotFoundException extends NotFoundException{
+    public CharacterNotFoundException(){
+        super(ErrorCode.CHARACTER_NOT_FOUND);
+    }
+}
