cf3c22206ecca3b4f7418817523f91e5d5c1a992
EunbeenDev <ros4312@naver.com>
2024-08-12 13:59:26 +0900

    [FEAT] 카테고리별 아이템 리스트 조회 기능 구현

---
+++ src/main/java/com/noplanb/domain/item/dto/response/CategoryItemRes.java
@@ -0,0 +1,36 @@
+package com.noplanb.domain.item.dto.response;
+
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class CategoryItemRes {
+    @Schema(type = "Long", example = "10", description = "아이템 ID")
+    private Long itemId;
+
+    @Schema(type = "String", example = "베레모", description = "아이템 이름")
+    private String itemName;
+
+    @Schema(type = "String", example = "베레모.png", description = "아이템 이미지")
+    private String itemImage;
+
+    @Schema(type = "String", example = "모자", description = "아이템 타입")
+    private String itemType;
+
+    @Schema(type = "boolean", example = "false", description = "장착 가능여부 ( = 잠금해제여부)")
+    private boolean ableToEquip;
+
+    @Schema(type = "boolean", example = "false", description = "장착 여부")
+    private boolean isEquipped;
+
+    @Schema(type = "Long", example = "20", description = "필요 레벨")
+    private Long requiredLevel;
+
+}
