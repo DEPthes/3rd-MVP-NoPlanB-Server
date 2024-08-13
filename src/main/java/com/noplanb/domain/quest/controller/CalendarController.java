65c65caaf2f09afd7250357c8081983b3262bbd5
lsn5963 <lsn5963@naver.com>
2024-08-13 20:42:59 +0900

    [FEAT] 달력 금일 이후 퀘스트 생성하기

--- src/main/java/com/noplanb/domain/quest/controller/CalendarController.java
+++ src/main/java/com/noplanb/domain/quest/controller/CalendarController.java
@@ -1,6 +1,7 @@
 package com.noplanb.domain.quest.controller;
 
 import com.noplanb.domain.quest.application.CalendarService;
+import com.noplanb.domain.quest.dto.req.CreateQuestAfterTodayReq;
 import com.noplanb.domain.quest.dto.res.RetrieveCalendarRes;
 import com.noplanb.global.config.security.token.CurrentUser;
 import com.noplanb.global.config.security.token.UserPrincipal;
@@ -16,10 +17,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
 import java.time.YearMonth;
 
 @Controller
@@ -38,5 +38,16 @@ public class CalendarController {
             @Parameter(example = "2024-08", description = "날짜를 입력해주세요", required = true) @PathVariable("date") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) YearMonth date,
             @Parameter(description = "Access Token을 입력해주세요.", required = true) @CurrentUser UserPrincipal userPrincipal){
         return calendarService.retrieveCalendar(date,userPrincipal);
+    }
+    @PostMapping
+    @Operation(summary = "달력 금일기준 이후 퀘스트생성", description = "달력 금일기준 이후 퀘스트생성")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "달력 금일기준 이후 퀘스트생성 성공", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = RetrieveCalendarRes.class))}),
+            @ApiResponse(responseCode = "400", description = "달력 금일기준 이후 퀘스트생성 실패", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))}),
+    })
+    public ResponseEntity<?> createQuestAfterToday(
+            @Parameter(description = "달력 금일기준 이후 퀘스트생성 dto Req입니다.", required = true) @RequestBody CreateQuestAfterTodayReq createQuestAfterTodayReq,
+            @Parameter(description = "Access Token을 입력해주세요.", required = true) @CurrentUser UserPrincipal userPrincipal){
+        return calendarService.createQuestAfterToday(createQuestAfterTodayReq,userPrincipal);
     }
 }
