7bd26db5544ad8ad010d55d4b0f0da23cfbf1210
lsn5963 <77337977+lsn5963@users.noreply.github.com>
2024-08-16 00:09:20 +0900

    [FIX] 퀘스트 완료 로직 수정

---
+++ src/main/java/com/noplanb/domain/quest/dto/res/RetrieveCompleteQuest.java
@@ -0,0 +1,16 @@
+package com.noplanb.domain.quest.dto.res;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+@Builder
+public class RetrieveCompleteQuest {
+    @Schema(type = "String", example = "완료", description = "완료한 퀘스트의 상태")
+    private String questType;
+    @Schema(type = "List", example = "uuid", description = "이미지 url")
+    private List<String> itemImageUrls;
+}
